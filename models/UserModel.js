157fc309d0a89f5fc4108f612d46942894af356c
Olha Sladkova <sladkovaoe@gmail.com>
2022-08-09 20:20:57 +0200

    fix

--- models/UserModel.js
+++ models/UserModel.js
@@ -3,6 +3,6 @@ const mongoose = require("mongoose");
 var UserSchema = mongoose.Schema({
   name: String,
   age: String
-});
+}, { collection: 'people' });
 
 module.exports = mongoose.model("people", UserSchema);
