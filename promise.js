66ce78d4495151293a986771fd7056c95924a439
Alex Vakula <albanec777@meta.ua>
2022-09-13 20:08:08 +0200

    arrey methodes

--- promise.js
+++ promise.js
@@ -1,116 +1,131 @@
-let promise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("1");
-  }, 4000);
-});
+// let promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("1");
+//   }, 4000);
+// });
 
-promise.then(
-  (result) => {
-    console.log(result);
-    console.log("2");
-    console.log("3");
-  },
-  (error) => {
-    console.log(error);
-  }
-);
-//reading file with callback!
+// promise.then(
+//   (result) => {
+//     console.log(result);
+//     console.log("2");
+//     console.log("3");
+//   },
+//   (error) => {
+//     console.log(error);
+//   }
+// );
+// //reading file with callback!
 
-const fs = require("fs"),
-  fsPromise = require("fs").promises;
+// const fs = require("fs"),
+//   fsPromise = require("fs").promises;
 
-/*
-fs.readFile("file.txt", {encoding:'UTF-8'}, (err, data) => {
-  if(err){
-    console.log('Не можу прочитати файл!' + err);
-  }
-  else {
-    console.log(data);
-  }
-});
-*/
-const getFile = (filename) => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(filename, { encoding: "UTF-8" }, (err, data) => {
-      resolve(data);
-    });
-  });
-};
+// /*
+// fs.readFile("file.txt", {encoding:'UTF-8'}, (err, data) => {
+//   if(err){
+//     console.log('Не можу прочитати файл!' + err);
+//   }
+//   else {
+//     console.log(data);
+//   }
+// });
+// */
+// const getFile = (filename) => {
+//   return new Promise((resolve, reject) => {
+//     fs.readFile(filename, { encoding: "UTF-8" }, (err, data) => {
+//       resolve(data);
+//     });
+//   });
+// };
 
-getFile("file copy.txt").then((data) => {
-  console.log(data);
-});
+// getFile("file copy.txt").then((data) => {
+//   console.log(data);
+// });
 
-fsPromise.readFile("file.txt", { encoding: "UTF-8" }).then((data) => {
-  console.log(data);
-});
+// fsPromise.readFile("file.txt", { encoding: "UTF-8" }).then((data) => {
+//   console.log(data);
+// });
 
-/*function Job(state) {
-  return new Promise(function (resolve, reject) {
-    console.log(state);
-    if (state) {
-      resolve(1);
-    } 
-    else {
-      reject(2);
-    }
-  });
-}
+// /*function Job(state) {
+//   return new Promise(function (resolve, reject) {
+//     console.log(state);
+//     if (state) {
+//       resolve(1);
+//     } 
+//     else {
+//       reject(2);
+//     }
+//   });
+// }
 
-Job()
-  .then(
-    (res) => console.log(res),
-    (rej) => console.log(rej)
-  )
-  .then(() => console.log("we are here"))
-  .catch((error) => console.log(error));
-*/
+// Job()
+//   .then(
+//     (res) => console.log(res),
+//     (rej) => console.log(rej)
+//   )
+//   .then(() => console.log("we are here"))
+//   .catch((error) => console.log(error));
+// */
 
-fsPromise
-  .writeFile("file.txt", "put TEXT in the FIle", { encoding: "UTF-8" })
-  .then(() => {
-    console.log("Writing is succeful");
-  });
+// fsPromise
+//   .writeFile("file.txt", "put TEXT in the FIle", { encoding: "UTF-8" })
+//   .then(() => {
+//     console.log("Writing is succeful");
+//   });
 
-fsPromise
-  .appendFile("file2.txt", "fghjkljhgfgfhjk", { encoding: "UTF-8" })
-  .then(() => {
-    console.log("Writing is succeful2");
-  });
+// fsPromise
+//   .appendFile("file2.txt", "fghjkljhgfgfhjk", { encoding: "UTF-8" })
+//   .then(() => {
+//     console.log("Writing is succeful2");
+//   });
 
-const { response, application } = require("express");
-const fetch = require("node-fetch");
+// const { response, application } = require("express");
+// const fetch = require("node-fetch");
 
-fetch("https://russianwarship.rip/api/v1/statistics/latest", {
-  method: "GET",
-  headers: { "Content-Type": "application/json" },
-})
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log("killed enemy " + data.data.stats.personnel_units);
+// fetch("https://russianwarship.rip/api/v1/statistics/latest", {
+//   method: "GET",
+//   headers: { "Content-Type": "application/json" },
+// })
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log("killed enemy " + data.data.stats.personnel_units);
     
     
-    console.log(undefined == null);
-    if (5 > 1){
-      console.log("Hi");
-    }
-    else {
-      console.log("Bye");
-    } 
+//     console.log(undefined == null);
+//     if (5 > 1){
+//       console.log("Hi");
+//     }
+//     else {
+//       console.log("Bye");
+//     } 
 
-    (5 < 1) ? console.log("Hi") : console.log("Bye");
+//     (5 < 1) ? console.log("Hi") : console.log("Bye");
 
-    console.log(Boolean(''));
-    console.log(Number(''));
+//     console.log(Boolean(''));
+//     console.log(Number(''));
 
-    var Users = {name:"Mark", age:"32", name2:"Alex", age2:"65"};
-    /*Users.forEach(function(item, i, arr){
-      console.log(item, i, arr);
-    })*/
+//     var Users = {name:"Mark", age:"32", name2:"Alex", age2:"65"};
+//     /*Users.forEach(function(item, i, arr){
+//       console.log(item, i, arr);
+//     })*/
     
-    for(var key in Users) {
-      console.log(key + " " + Users[key]);
-    };
-  });
+//     for(var key in Users) {
+//       console.log(key + " " + Users[key]);
+//     };
+//   });
+
+  var Users = [{name:"Mark", age:"32"}, {name:"Alex", age:"65"}, {name:"Alsafasf", age:"62"}, {name:"Tghfgg", age:"15"}];
+  var Fruits = ["Peach", "Apple", "Pear", "Peach"];
+  console.log(Fruits.indexOf("Peach"));
+
+  var User = Users.find(item => item.age > 60);
+  console.log(User);
+
+  var UsersUnder60 = Users.filter(item => item.age < 60);
+  console.log(UsersUnder60);
+
+
+  var arrMap = Users.map( item => item.name + '!');
+  console.log(arrMap);
+  console.log(Users);
